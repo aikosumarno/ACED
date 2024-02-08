b056d63c46bbb232e2b8e35f6103c2c1ba51a19d
aikosumarno <asumarno@students.cs.ubc.ca>
2024-02-07 22:03:08 -0800

    Added tests for testCard class and added methods to change the variables in Card class.

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -23,4 +23,24 @@ public class Card {
     public boolean getStatus() {
         return false; //stub
     }
+
+    // MODIFIES: this
+    // EFFECTS: changes question to the new question
+    public String editQuestion(String newQuestion) {
+        return null; //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes answer to the new answer
+    public String editAnswer(String newAnswer) {
+        return null; //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set status of card with new status
+    //          false indicating that it has not been mastered
+    //          and true indicating that it has been mastered
+    public boolean changeStatus(boolean newStatus) {
+        return false; //stub
+    }
 }
--- src/test/model/CardTest.java
+++ src/test/model/CardTest.java
@@ -3,30 +3,74 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CardTest {
+    private Card happy;
+    private Card brave;
+    private Card love;
 
     @BeforeEach
     void runBefore() {
-
+        happy = new Card("happy", "feeling content");
+        brave = new Card("brave", "to be courageous");
+        love = new Card("love", "deep affection");
     }
 
     @Test
     void testCard() {
-
+        assertEquals("happy", happy.getQuestion());
+        assertEquals("feeling content", happy.getAnswer());
+        assertFalse(happy.getStatus());
     }
 
     @Test
     void testGetQuestion() {
-
+        assertEquals("happy", happy.getQuestion());
+        assertEquals("brave", brave.getQuestion());
+        assertEquals("love", love.getQuestion());
     }
 
     @Test
-    void testgetAnswer() {
-
+    void testGetAnswer() {
+        assertEquals("feeling content", happy.getAnswer());
+        assertEquals("to be courageous", brave.getAnswer());
+        assertEquals("deep affection", love.getAnswer());
     }
 
     @Test
     void testGetStatus() {
+        assertFalse(love.getStatus());
+        love.changeStatus(true);
+        assertTrue(love.getStatus());
+        love.changeStatus(false);
+        assertFalse(love.getStatus());
+    }
 
+    @Test
+    void testEditQuestion() {
+        love.editQuestion("fond");
+        assertEquals("fond", love.getQuestion());
+        love.editQuestion("love");
+        assertEquals("love", love.getQuestion());
+        brave.editQuestion("fearless");
+        assertEquals("fearless", brave.getQuestion());
+    }
+
+    @Test
+    void testEditAnswer() {
+        love.editAnswer("like very much");
+        assertEquals("like very much", love.getAnswer());
+        happy.editAnswer("feeling cheerful");
+        assertEquals("feeling cheerful", happy.getAnswer());
+    }
+
+    @Test
+    void testSetStatus() {
+        assertFalse(brave.getStatus());
+        brave.changeStatus(true);
+        assertTrue(brave.getStatus());
+        brave.changeStatus(false);
+        assertFalse(brave.getStatus());
     }
 }
