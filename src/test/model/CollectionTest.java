0dee034237b328dd341bc2af6dd17d363f8f13ae
aikosumarno <asumarno@students.cs.ubc.ca>
2024-02-07 23:09:42 -0800

    Added tests for CollectionTest

--- src/test/model/CollectionTest.java
+++ src/test/model/CollectionTest.java
@@ -3,20 +3,32 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CollectionTest {
+    private Deck subjects;
+    private Deck emotions;
+    private Collection myCollection;
 
     @BeforeEach
     void runBefore() {
-
+        subjects = new Deck("subjects");
+        emotions = new Deck("emotions");
+        myCollection = new Collection();
     }
 
     @Test
     void testCollection() {
-
+        assertEquals(null, myCollection.getCollection());
     }
 
     @Test
     void testAddNewDeck() {
-
+        myCollection.addNewDeck(subjects);
+        assertEquals(1, myCollection.getCollection().size());
+        assertEquals(subjects, myCollection.getCollection().get(0));
+        myCollection.addNewDeck(emotions);
+        assertEquals(2, myCollection.getCollection().size());
+        assertEquals(emotions, myCollection.getCollection().get(1));
     }
 }
