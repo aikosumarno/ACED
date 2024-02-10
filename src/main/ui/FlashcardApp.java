26e523a9f31ebb36bc963cc4766022a54f69cac4
aikosumarno <asumarno@students.cs.ubc.ca>
2024-02-09 23:24:03 -0800

    Added FlashcardApp class in ui package

---
+++ src/main/ui/FlashcardApp.java
@@ -0,0 +1,25 @@
+package ui;
+
+import model.Card;
+import model.Collection;
+import model.Deck;
+
+public class FlashcardApp {
+    private Card happy;
+    private Card sad;
+    private Card angry;
+    private Card curious;
+    private Card love;
+    private Card maths;
+    private Card science;
+    private Deck emotions;
+    private Deck subjects;
+    private Collection myCollection;
+
+    public FlashcardApp() {
+        runFlashcard();
+    }
+
+    private void runFlashcard() {
+    }
+}
