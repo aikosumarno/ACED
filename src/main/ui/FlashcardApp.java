f2415d792a01f950e8ba8f2fb4a3693f8441295b
aikosumarno <asumarno@students.cs.ubc.ca>
2024-02-12 16:26:34 -0800

    Added ui for users to add new deck, add new cards, edit existing card, and delete card.

--- src/main/ui/FlashcardApp.java
+++ src/main/ui/FlashcardApp.java
@@ -4,19 +4,9 @@ import model.Card;
 import model.Collection;
 import model.Deck;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FlashcardApp {
-    private Card happy;
-    private Card sad;
-    private Card angry;
-    private Card curious;
-    private Card love;
-    private Card maths;
-    private Card science;
-    private Deck emotions;
-    private Deck subjects;
     private Collection collection;
 
     private Scanner input;
@@ -27,12 +17,12 @@ public class FlashcardApp {
 
     private void runFlashcard() {
         boolean keepStudying = true;
-        String command = null;
+        String command;
 
         init();
 
         while (keepStudying) {
-            displayDeckMenu();
+            displayCollectionMenu();
             command = input.next();
             command = command.toLowerCase();
 
@@ -61,7 +51,7 @@ public class FlashcardApp {
     // EFFECTS: displays menu of decks users can choose from in collection or create a new deck
     private void displayCollectionMenu() {
         System.out.println("\nMenu:");
-        System.out.println("\ts -> select existing card");
+        System.out.println("\ts -> select existing deck");
         System.out.println("\ta -> add new deck");
         System.out.println("\tq -> quit");
     }
@@ -86,25 +76,107 @@ public class FlashcardApp {
     // MODIFIES: this
     // EFFECTS: adds a new Deck
     private void addDeck() {
-        String name = input.nextLine();
+        System.out.println("Enter Deck Name: ");
+        String name = input.next();
         Deck newDeck = new Deck(name);
         collection.addNewDeck(newDeck);
+        System.out.println(name + " deck has been added to collection.");
     }
 
-    // EFFECTS: prompts user to select an existing deck and returns it
-    private Deck selectDeck() {
+    // EFFECTS: prompts user to select an existing deck and redirects to their choice
+    private void selectDeck() {
         int choice = 0;
 
 
-        while (choice > collection.getCollection().size() || choice <= 0) {
-            int count = 1;
+        while (choice > collection.getCollection().size() + 1 || choice <= 0) {
+            System.out.println("\n1. return to main menu");
+            int count = 2;
             for (Deck current: collection.getCollection()) {
-                System.out.println("\n" + count + "->" +  current.getName());
+                System.out.println(count + ". " +  current.getName());
                 count++;
             }
             choice = input.nextInt();
         }
 
-        return collection.getCollection().get(choice - 1);
+        if (choice != 1) {
+            viewDeck(collection.getCollection().get(choice - 2));
+        }
+    }
+
+    // EFFECTS: prints out all of the cards in the chosen deck
+    private void viewDeck(Deck deck) {
+        int num = 1;
+
+        for (Card current: deck.viewDeck()) {
+            System.out.println(num + ". " + current.getQuestion() + ": " + current.getAnswer());
+            num++;
+        }
+
+        selectDeckActions(deck);
+    }
+
+    private void selectDeckActions(Deck deck) {
+        String choice = "";
+        boolean validChoice = false;
+
+        while (!validChoice) {
+            displayDeckMenu();
+            choice = input.next();
+            choice = choice.toLowerCase();
+            if (choice.equals("a") || choice.equals("e") || choice.equals("d") || choice.equals("r")) {
+                validChoice = true;
+            }
+        }
+
+        if (choice.equals("a")) {
+            addCard(deck);
+        } else if (choice.equals("e")) {
+            editCard(deck);
+        } else if (choice.equals("d")) {
+            deleteCard(deck);
+        }
+    }
+
+    private void addCard(Deck deck) {
+        System.out.println("Enter Question: ");
+        String question = input.next();
+        System.out.println("Enter Answer: ");
+        String answer = input.next();
+        Card newCard = new Card(question, answer);
+        deck.addCard(newCard);
+        System.out.println("Card has been added to deck.");
+        viewDeck(deck);
+    }
+
+    private void editCard(Deck deck) {
+        int num = 0;
+
+        while (num <= 0 || num > deck.getSize()) {
+            System.out.println("Enter number of card you would like to edit:");
+            num = input.nextInt();
+        }
+
+        num = num - 1;
+        System.out.println("Enter New Question: ");
+        String newQuestion = input.next();
+        System.out.println("Enter New Answer: ");
+        String newAnswer = input.next();
+
+        deck.viewDeck().get(num).editQuestion(newQuestion);
+        deck.viewDeck().get(num).editAnswer(newAnswer);
+        viewDeck(deck);
+    }
+
+    private void deleteCard(Deck deck) {
+        int num = 0;
+
+        while (num <= 0 || num > deck.getSize()) {
+            System.out.println("Enter number of card you would like to delete:");
+            num = input.nextInt();
+        }
+        num = num - 1;
+
+        deck.deleteCard(deck.viewDeck().get(num));
+        viewDeck(deck);
     }
 }
