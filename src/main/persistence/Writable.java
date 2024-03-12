09cabcda31fbabcf9394ce84fcd0c561a3a7689b
aikosumarno <asumarno@students.cs.ubc.ca>
2024-03-12 09:42:51 -0700

    added class level documentation

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// ensures that all the class are able to write to the JSON file.
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+// Creates a new flashcard app
 public class Main {
     public static void main(String[] args) {
         new FlashcardApp();
