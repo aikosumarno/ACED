7b9aa5d55e97a0241e117ad3d895cd1ac03db477
aikosumarno <asumarno@students.cs.ubc.ca>
2024-04-03 14:35:57 -0700

    added tests for event

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -6,13 +6,15 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Event class
  */
 public class EventTest {
 	private Event e;
+    private Event e2;
+    private Event e3;
 	private Date d;
 	
 	//NOTE: these tests might fail if time at which line (2) below is executed
@@ -22,6 +24,7 @@ public class EventTest {
 	@BeforeEach
 	public void runBefore() {
 		e = new Event("Deleted 'Define happy' card from emotions deck.");   // (1)
+        e2 = new Event("Added 'Define angry' card to emotions deck.");
 		d = Calendar.getInstance().getTime();   // (2)
 	}
 	
@@ -35,4 +38,17 @@ public class EventTest {
 	public void testToString() {
 		assertEquals(d.toString() + "\n" + "Deleted 'Define happy' card from emotions deck.", e.toString());
 	}
+
+    @Test
+    public void testEquals() {
+        e3 = e;
+        assertFalse(e.equals(e2));
+        assertFalse(e.equals(d));
+        assertTrue(e.equals(e3));
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(13 * e.getDate().hashCode() + e.getDescription().hashCode(), e.hashCode());
+    }
 }
